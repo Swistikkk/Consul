6321e3ae8f478862f6689f9f11ab8258e9d44772
Мама <>
2016-08-02 17:44:48 +0300

    git init first files

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var scss = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+
+gulp.task('scss', function(){
+  return gulp.src('app/sass/style.scss')
+   .pipe(scss(
+     //{outputStyle: 'compressed'}
+   ).on('error', scss.logError))
+   .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('browser-sync', function(){
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  });
+});
+
+gulp.task('watch', ['browser-sync'], function(){
+  gulp.watch('app/sass/**/*.scss', ['scss']);
+  gulp.watch('app/css/**/*.css', browserSync.reload);
+  gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
